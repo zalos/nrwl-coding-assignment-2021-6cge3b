23c0b84031d56b612a29583fa10f00db5d2bbf9c
Jared Kremer <jared.t.kremer@gmail.com>
2021-08-30 10:33:38 -0400

    append snackbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActionsSubject } from '@ngrx/store';
+import { filter, skip } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private _actions: ActionsSubject, private _snackBar: MatSnackBar) {
+    // this._actions.pipe(
+    //   filter( action =>  action.type.indexOf('Success') != -1 || action.type.indexOf('Failure') != -1 ) // optional: skips initial logging done by ngrx
+    // ).subscribe((action) => this.runSnackBar(action));
+  }
+
+  private runSnackBar(action: any) {
+      this._snackBar.open("message", action);
+  }
+}
